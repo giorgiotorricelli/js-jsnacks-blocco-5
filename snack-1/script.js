dca687cfa53a8e78f8c46e09fa006ae5f57bcf85
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-14 15:49:32 +0200

    corretto snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,11 +1,22 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
-const output = [];
+
+//si potrebbe usare semplicemente il metodo join()
+console.log(names.join(', '));
+
+//oppure il forEach()
+let output = ``;
 
 names.forEach(element => {
-    output.push(`'${element}'`);
+    
+    if (output.length === 0){
+        output += element;
+    } else {
+        output += `, ${element}`;
+    }
+    
 });
 
-console.log(output.join(', '));
+console.log(output);
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
