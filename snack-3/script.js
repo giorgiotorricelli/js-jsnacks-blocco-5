97f292e1b3f8d6f8e31b31abb189ec76bd16f1d1
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-14 16:07:59 +0200

    snack 3 completato

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,14 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+//qua mi conviene usare ancora la map
+
+function cbNumbers(numb, index){
+    return numb + 1;  //incrementa ogni numero di uno
+}
+
+const output = numbers.map(cbNumbers); //restituisce il nuovo array
+console.log(output);
+
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
