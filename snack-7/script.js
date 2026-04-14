b723e3fd295e72ae5546cc7397e0896d27138610
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-14 16:43:23 +0200

    completato snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -19,5 +19,17 @@ const students = [
   },
 ];
 
+//uso filter per filtrare tra gli id
+
+function cbStudents(student, index){
+  return student.id === 2;
+}
+
+const output = students.filter(cbStudents);
+const output2 = students.filter(student => student.id === 2)
+
+console.log(output, output2);
+
+
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
