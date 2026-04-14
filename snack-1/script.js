6659d796f6cbf057397cab3d44c39a0529e5e5c8
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-14 15:33:30 +0200

    snack 1 completato

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,11 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
+const output = [];
+
+names.forEach(element => {
+    output.push(`'${element}'`);
+});
+
+console.log(output.join(', '));
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
